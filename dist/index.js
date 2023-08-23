44b074f72d1a115b68a94da0c8b5016318851353
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-08-24 00:55:10 +0200

    fix spawn process

--- dist/index.js
+++ dist/index.js
@@ -2797,13 +2797,21 @@ const logError = (err, output) => {
     return err;
 };
 const spawnProcess = async (command, args, cwd) => {
-    const childProcess = external_child_process_namespaceObject.spawn(command, args, { stdio: 'inherit', cwd });
+    const childProcess = external_child_process_namespaceObject.spawn(command, args, { cwd });
     return new Promise((resolve, reject) => {
         const output = [];
-        childProcess.stdout?.on('data', (d) => output.push(d.toString()));
-        childProcess.stderr?.on('data', (d) => output.push(d.toString()));
+        const r1 = childProcess.stdout?.on('data', (d) => output.push(d.toString()));
+        const r2 = childProcess.stderr?.on('data', (d) => output.push(d.toString()));
+        const p1 = new Promise((resolve) => (r1 ? r1.once('close', resolve) : resolve()));
+        const p2 = new Promise((resolve) => (r2 ? r2.once('close', resolve) : resolve()));
         childProcess.once('error', (err) => reject(logError(err, output)));
-        childProcess.once('exit', (code) => (code === 0 ? resolve(output.join('')) : reject(logError(code, output))));
+        childProcess.once('exit', async (code) => {
+            r1?.removeAllListeners('data');
+            r2?.removeAllListeners('data');
+            await p1;
+            await p2;
+            return code === 0 ? resolve(output.join('')) : reject(logError(code, output));
+        });
     });
 };
 
--- src/spawnProcess.ts
+++ src/spawnProcess.ts
@@ -6,13 +6,22 @@ const logError = (err: unknown, output: string[]) => {
 }
 
 export const spawnProcess = async (command: string, args: string[], cwd: string) => {
-    const childProcess = child_process.spawn(command, args, { stdio: 'inherit', cwd })
+    const childProcess = child_process.spawn(command, args, { cwd })
     return new Promise<string>((resolve, reject) => {
         const output: string[] = []
-        childProcess.stdout?.on('data', (d) => output.push(d.toString()))
-        childProcess.stderr?.on('data', (d) => output.push(d.toString()))
+        const r1 = childProcess.stdout?.on('data', (d) => output.push(d.toString()))
+        const r2 = childProcess.stderr?.on('data', (d) => output.push(d.toString()))
+
+        const p1 = new Promise<void>((resolve) => (r1 ? r1.once('close', resolve) : resolve()))
+        const p2 = new Promise<void>((resolve) => (r2 ? r2.once('close', resolve) : resolve()))
 
         childProcess.once('error', (err) => reject(logError(err, output)))
-        childProcess.once('exit', (code: unknown) => (code === 0 ? resolve(output.join('')) : reject(logError(code, output))))
+        childProcess.once('exit', async (code: unknown) => {
+            r1?.removeAllListeners('data')
+            r2?.removeAllListeners('data')
+            await p1
+            await p2
+            return code === 0 ? resolve(output.join('')) : reject(logError(code, output))
+        })
     })
 }
